cb5f3690e26e9e499ae59aaf07f0c5ba2e658195
Patrick <patrickbellot77@gmail.com>
2017-08-25 13:47:28 -0500

    initial commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+  
+  var colors = [UIColor]()
+  
+  @IBOutlet weak var postcard: UIImageView!
+  @IBOutlet weak var colorSelection: UICollectionView!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    colors += [.black, .gray, .white, .orange, .red, .magenta, .purple, .blue, .cyan, .green]
+    
+    for i in 0...9 {
+      for j in 1...10 {
+        let color = UIColor(hue: CGFloat(i) / 10, saturation: CGFloat(j) / 10, brightness: 1, alpha: 1)
+        colors.append(color)
+      }
+    }
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return colors.count
   }
-
-
-}
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+    
+    cell.backgroundColor = colors[indexPath.row]
+    cell.layer.borderWidth = 1
+    cell.layer.cornerRadius = 5
+    
+    return cell
+  }
+  
+  
+}//end of class
 
