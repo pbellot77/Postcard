f17f8941e33ccec09a4a686f11138ba815846d5c
Patrick <patrickbellot77@gmail.com>
2017-08-25 14:32:47 -0500

    set up UI

--- Postcard/FontsViewController.swift
+++ Postcard/FontsViewController.swift
@@ -9,27 +9,25 @@
 import UIKit
 
 class FontsViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
+  let fonts = UIFont.familyNames.sorted()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return fonts.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    let fontName = fonts[indexPath.row]
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    cell.textLabel?.text = fontName
+    cell.textLabel?.font = UIFont(name: fontName, size: 18)
+    
+    return cell
+  }
+  
 }
--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
   
-  var colors = [UIColor]()
-  
   @IBOutlet weak var postcard: UIImageView!
   @IBOutlet weak var colorSelection: UICollectionView!
   
+  var colors = [UIColor]()
+  var image: UIImage?
+  var topText = "Visit London"
+  var topFontName = "Helvetica Neue"
+  var topColor = UIColor.white
+  var bottomText = "Home of Sherlock Holms, Paddington Bear, and James Bond"
+  var bottomFontName = "Helvetica Neue"
+  var bottomColor = UIColor.white
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -27,6 +33,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         colors.append(color)
       }
     }
+    renderPostcard()
   }
   
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -42,6 +49,59 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     return cell
   }
+  
+  func renderPostcard() {
+    
+    // 1 - define the total drawing area
+    let drawRect = CGRect(x: 0, y: 0, width: 3000, height: 2400)
+    
+    // 2 - define where to draw the top and bottom text
+    let topTextRect = CGRect(x: 250, y: 200, width: 2500, height: 800)
+    let bottomTextRect = CGRect(x: 250, y: 1800, width: 2500, height: 600)
+    
+    // 3 - create UIFont instances out of the font names, providing fallbacks on failure
+    let topFont = UIFont(name: topFontName, size: 350) ?? UIFont.systemFont(ofSize: 250)
+    let bottomFont = UIFont(name: bottomFontName, size: 150) ?? UIFont.systemFont(ofSize: 100)
+    
+    // 4 - create a centered paragraph style
+    let centered = NSMutableParagraphStyle()
+    centered.alignment = .center
+    
+    // 5 - wrap that in attributed strings with the user's colors
+    let topTextAttributes: [NSAttributedStringKey: Any] = [.foregroundColor: topColor, .font: topFont, .paragraphStyle: centered]
+    let bottomTextAttributes: [NSAttributedStringKey: Any] = [.foregroundColor: bottomColor, .font: bottomFont, .paragraphStyle: centered]
+    
+    // 6 - start redering at the correct size
+    let renderer = UIGraphicsImageRenderer(size: drawRect.size)
+    
+    postcard.image = renderer.image(actions: { ctx in
+      
+      //fill the entire screen in gray
+      UIColor.gray.set()
+      ctx.fill(drawRect)
+      
+      // 8 - draw the user's image at the top-left corner
+      image?.draw(at: CGPoint(x: 0, y: 0))
+      
+      // 9 - draw the top and bottom text
+      topText.draw(in: topTextRect, withAttributes: topTextAttributes)
+      bottomText.draw(in: bottomTextRect, withAttributes: bottomTextAttributes)
+    })
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
   
 }//end of class
